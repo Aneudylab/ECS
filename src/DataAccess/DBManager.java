4f1e34ca598038cb6ad92b5083c430ce7d38b73b
LuisaIsabel <a_nyelia@hotmail.com>
2013-09-09 17:33:29 +0200

    Update DBManager.java
    Ahora tiene los metodos ejecutarUpdate y ejecutarQuery. y no hay que estar embromando con el PreparedStatement en la clase UsuarioDA.

--- src/DataAccess/DBManager.java
+++ src/DataAccess/DBManager.java
@@ -3,6 +3,8 @@ package DataAccess;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * Description: Super class for script helper
@@ -16,10 +18,12 @@ public class DBManager {
 
 	private static Connection connection = null;
 	private static DBManager dbManager;
+	private static PreparedStatement stm;
+    private static ResultSet resultado;
 	private String driver = "com.mysql.jdbc.Driver";
-	private String url = "jdbc:mysql://localhost:3306/ecs";
+	private String url = "jdbc:mysql://localhost/ecs";
 	private String user = "root";
-	private String pwd = "aneudylab";
+	private String pwd = "";
 
 	private DBManager() {
 		if (connection == null) {
@@ -30,7 +34,7 @@ public class DBManager {
 				connection = DriverManager.getConnection(url, user, pwd);
 
 				if (connection != null) {
-					System.out.println("Conexión establecida... ");
+					System.out.println("ConexiÃ³n establecida... ");
 				}
 
 			} catch (IllegalAccessException e) {
@@ -54,6 +58,59 @@ public class DBManager {
 			}
 		}
 	}
+	
+	
+	/**
+	 * 
+	 * @param consulta Query
+	 * @param parametros de la consulta, si los hay
+	 * @return los resultados 
+	 * @throws Exception
+	 */
+	
+	public static ResultSet ejecutarQuery(String consulta, Object[] parametros)throws Exception{
+        stm = connection.prepareStatement(consulta);
+        
+        if(parametros != null){
+            for(int k = 0; k < parametros.length; k++){
+                stm.setObject(k+1, parametros[k]);
+            }
+        }
+        resultado = stm.executeQuery();
+        
+     return resultado;
+ 
+	}
+	
+	
+	
+	/**
+	 * Metodo para ejecutar un insert o actualizacion
+	 * @param consulta es el Query
+	 * @param parametros Los parametros que tiene el Query, si los hay
+	 * @throws Exception
+	 */
+	
+	public static void ejecutarUpdate(String consulta, Object[] parametros) throws Exception{
+        stm = connection.prepareStatement(consulta);
+        
+        if(parametros != null){
+            for(int k = 0; k < parametros.length; k++){
+                stm.setObject(k+1, parametros[k]);
+            }
+        }
+        
+        stm.executeUpdate();
+  
+    }
+	
+	
+	
+	/**
+	 * Metodo para abrir la conexion con la base de datos
+	 * @return la conexion
+	 */
+	
 
 	public static Connection openDBConnection() {
 		try {
@@ -66,11 +123,19 @@ public class DBManager {
 
 	}
 	
+	
+	
+	/**
+	 * Metodo para cerrar la conexion con la Base De Datos
+	 */
+	
 	public static void closeDBConnection (){
 	   
 	   	try {
-			connection.close();
-			System.out.println("Conexión cerrada... ");
+	   		if(!connection.isClosed()){
+	            connection.close();
+	            System.out.println("ConexiÃ³n cerrada... ");
+	        }
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 
@@ -79,3 +144,5 @@ public class DBManager {
 	}
 
 }
+
+
