fe88b2894dc95832476b9d0b5cd8815f530c20a3
LuisaIsabel <a_nyelia@hotmail.com>
2013-09-09 17:38:49 +0200

    Update UsuarioDA.java
    Utilizacion de los metodos creados...

--- src/DataAccess/UsuarioDA.java
+++ src/DataAccess/UsuarioDA.java
@@ -2,8 +2,7 @@ package DataAccess;
 
 import domain.Usuario;
 import java.sql.ResultSet;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
+
 
 public class UsuarioDA {
 
@@ -12,37 +11,30 @@ public class UsuarioDA {
 
 	public void leerUsuario(Usuario usu) {
 
-		Connection con = DBManager.openDBConnection();
-		String usuario = usu.getNombreUsuario();
-		String clave = usu.getClave();
+		try{
 		
-		// creando mi query de verificacion
-		
-        String SQLquery = "SELECT id_usuario, nombre, r.descripcion rol "+
-                          " FROM usuario u, rol r " + 
-                          "WHERE u.id_rol = r.id_rol AND nombre_usuario= ? And CLAVE= ?";
-				
-		try {
-
-			PreparedStatement stmt = con.prepareStatement(SQLquery);
-			int i = 0;
-			stmt.setString(++i,usuario);
-			stmt.setString(++i,clave);
-			ResultSet result = stmt.executeQuery();
-
-		// verificando si el usuario existe, Si no el id se mantienen en 0
+		     ResultSet result;								
+                     Object[] parametros;			
 			
-			if (result.next()) {
-                usu.setID(result.getInt("id_usuario"));
-                usu.setNombre(result.getString("nombre"));
-                usu.setRol(result.getString("rol"));
-			}
-		}
+		     String Select = "SELECT id_usuario, nombre, r.descripcion rol "+
+                                     "FROM usuario u, rol r " + 
+                                     "WHERE u.id_rol = r.id_rol AND nombre_usuario= ? And CLAVE= ?";
+						
+	             parametros = new Object[]{ usu.getNombreUsuario(), usu.getClave() };
+				
+		     DBManager.openDBConnection();
+                     result = DBManager.ejecutarQuery(Select, parametros);
+		
+	 	   if(result.next()){				
+			 usu.setID(result.getInt("id_usuario"));
+                         usu.setNombre(result.getString("nombre"));
+                         usu.setRol(result.getString("rol"));
+	           }
 		
 		// Manejo de excepcion
         // TODo: Que jairis la maneje bien
 		
-		catch (Exception err) {
+		}catch (Exception err) {
 			System.out.println("Error: " + err.getMessage());
 		}
 	}
