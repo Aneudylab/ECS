c2b1095d0ad4ed78c46cad2fc3c4cee7cd43cc98
ericherrera <ericmanuel.herrera@gmail.com>
2013-09-23 22:32:52 -0400

    Crea el usuarioActual
    Esta sera la clase que se encargara de crear el usuarioActual si es
    encontrado en la DB

--- src/DataAccess/UsuarioDA.java
+++ src/DataAccess/UsuarioDA.java
@@ -7,38 +7,53 @@ import java.sql.ResultSet;
 
 
 public class UsuarioDA {
+    
+	////////////////////////
+	//  Costructor
+	///////////////////////
+	
+    public UsuarioDA() {}
 
-    public UsuarioDA() {
-    }
-
-    public void leerUsuario(Usuario usu) {
-
+    public Usuario leerUsuario(String nombreUsr, String clave) {
+        
+		Usuario usr = null; //creando el usuario que sera retornado
+							//incializandola como nula
+		
         try{
-            Rol rol;
-            ResultSet result;								
+            
+            ResultSet result;	
             Object[] parametros;			
 
             String Select = "SELECT id_usuario id,nombre,id_supervisor supervisor,rol.id_rol,descripcion rol " +
                             "FROM usuario INNER JOIN rol ON usuario.id_rol = rol.id_rol " + 
                             "WHERE nombre_usuario = ? And clave= ?";
 
-            parametros = new Object[]{ usu.getNombreUsuario(), usu.getClave() };
+            parametros = new Object[]{ nombreUsr, clave };
 
             DBManager.openDBConnection();
             result = DBManager.ejecutarQuery(Select, parametros);
 
             if(result.next()){			
-       			rol = new Rol(result.getInt("id_rol"),result.getString("rol"));
-                usu.setID(result.getInt("id"));
-                usu.setNombre(result.getString("nombre"));
-                usu.setRol(rol);
+       			
+				int idRol = result.getInt("id_rol"); //id del Rol
+				String descRol = result.getString("rol"); //descripcion del Rol 
+                
+				int idUsuario = result.getInt("id"); //Id del usuario
+                String nombre = result.getString("nombre"); //Nombre del usuario
+                
+				//construyendo los objetos Rol y Usuario
+				Rol rol = new Rol(idRol,descRol); 
+				usr = new Usuario(idUsuario,nombre,rol);
+				
             }
-
+            
+			return usr;
             // Manejo de excepcion
             // TODo: Que jairis la maneje bien
 
         }catch (Exception err) {
             System.out.println("Error: " + err.getMessage());
-        }
+		    return usr;
+		}
     }
 }
