889f6998d0b9024097c49eb0123b151036d73bf5
ericherrera <ericmanuel.herrera@gmail.com>
2013-09-23 22:34:11 -0400

    Esta clase se encargara asignar el usuario actual
    Se modifico para que iniciar sesion establezca si es el usuarioActual en
    su variable estatica

--- src/domain/ControladorSesion.java
+++ src/domain/ControladorSesion.java
@@ -8,24 +8,21 @@ import view.MainWindow;
 public class ControladorSesion {
 
     public static Usuario usuarioActual;
-	private String nombre = "";
-	private String clave = "";
 
-	public void iniciarSesion(String nombreUsuario, String Clave) {
+	public void iniciarSesion(String nombreUsr, String clave) {
 
-        Usuario usu = new Usuario(nombreUsuario, Clave);
+		setUsuarioActual(null);
 		UsuarioDA usuarioDA = new UsuarioDA();
-        usuarioDA.leerUsuario(usu);
+        Usuario unUsuario = usuarioDA.leerUsuario(nombreUsr,clave);
 
-        /* 
-         * Antes de llamar esta función el usuario actual se vuelve null
-         * Esto asegura que si la autenticación falla no habrá usuario actual.
-         */
-        ControladorSesion.usuarioActual = null;
-
-        if(usu.getID() != 0){
-            ControladorSesion.usuarioActual = usu;
+        if(unUsuario != null){
+           setUsuarioActual(unUsuario);
 		}
 	}
+	
+	//metodo de establecer el usuario
+	public void setUsuarioActual(Usuario unUsr){
+	    usuarioActual = unUsr;
+	}	
 
 }
