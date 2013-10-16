0657fcc059253923ed2fb0b9c5af7b5535e4fc32
ericherrera <ericmanuel.herrera@gmail.com>
2013-10-16 00:19:46 -0400

    Se creo la clase EvaluacionDA
    creada

---
+++ src/DataAccess/EvaluacionDA.java
@@ -0,0 +1,34 @@
+
+package DataAccess;
+
+import java.util.Date;
+import domain.Evaluacion;
+
+public class EvaluacionDA{
+
+    public void guardarEvaluacion(int idSuper, int idRepr, Evaluacion unaEva){
+	    
+		Object[] parametros;
+        String query = "INSERT INTO evaluacion " +
+                       "(fecha_creada,id_estado_evaluacion,id_plantilla," + 
+					   "id_representante,id_supervisor) " +
+					   "VALUES(?,?,?,?,?) "; 
+        
+        parametros = new Object[]{new Date(),1,unaEva.getIdPlantilla(),idRepr,idSuper};
+        
+        try{
+        	
+            DBManager.openDBConnection();
+			int id = DBManager.executeQueryGeneratedKeys(query, parametros);
+            
+			unaEva.setID(id);
+			
+        }catch(Exception ex){
+        	System.out.println("Error: " + ex.getMessage());
+        }
+		finally{
+		   DBManager.closeDBConnection();
+		}
+	   
+	}
+}
