c7fbc4adff182ebebe20bb5d6c921cb9f48892ff
ericherrera <ericmanuel.herrera@gmail.com>
2013-10-16 00:20:55 -0400

    Se creo la clase Evaluacion
    clase creada

---
+++ src/domain/Evaluacion.java
@@ -0,0 +1,61 @@
+
+package domain;
+
+import java.util.ArrayList;
+import DataAccess.RespuestaDA;
+
+
+public class Evaluacion{
+  
+   private int evaluacionid;
+   private Plantilla unaPlantilla;
+   private ArrayList<Respuesta> listaRespuestas;
+  
+   //---------------------------------------------------
+   //	Constructores
+   //------------------------------------------------
+   public Evaluacion(){}
+
+   public Evaluacion(Plantilla tmpPlantilla){
+		unaPlantilla = tmpPlantilla;
+		listaRespuestas = new ArrayList<Respuesta>();
+    }
+	
+	public Evaluacion(Plantilla tmpPlantilla, ArrayList<Respuesta> resp){
+	    unaPlantilla = tmpPlantilla;
+		listaRespuestas = resp;	    
+	}
+	
+   //---------------------------------------------------
+   //	Metodos
+   //------------------------------------------------
+   public int getId(){
+      return evaluacionid;
+   } 
+   
+   public int getIdPlantilla(){
+      return unaPlantilla.getId();
+   }
+	
+   public void crearRespuesta(int tmpNum, boolean tmpCumple){
+      listaRespuestas.add(new Respuesta(tmpNum,tmpCumple));
+   }
+   
+   public void setID (int tmpID){
+      evaluacionid = tmpID;
+   }
+   
+   public void guardarRespuestas(){
+     RespuestaDA unaResDA = new RespuestaDA();
+	 
+     for(Respuesta tmpRes:listaRespuestas){
+	    int idPunto = tmpRes.getID();
+		boolean cumple = tmpRes.getCumplePunto();
+		int idPlantilla = unaPlantilla.getId();
+		
+		unaResDA.guardarRespuesta(evaluacionid,unaPlantilla.getId(),
+		                          idPunto,cumple);
+	 }
+   }
+  
+}
