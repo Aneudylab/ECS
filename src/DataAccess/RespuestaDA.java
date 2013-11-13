423f24c027b45a67dde8820684b6c6616bb03397
Aneudy Labour <aneudylab@gmail.com>
2013-11-13 02:29:05 -0400

    RespuestaDA actualizada para CU004
    Se crearon los métodos necesarios para que RespuestaDA sea relevante en el
    CU004.

--- src/DataAccess/RespuestaDA.java
+++ src/DataAccess/RespuestaDA.java
@@ -1,5 +1,7 @@
 
 package DataAccess;
+import domain.Evaluacion;
+import java.sql.ResultSet;
 
 public class RespuestaDA{
 
@@ -7,15 +9,13 @@ public class RespuestaDA{
                                  int idPtoEva,boolean cumplePunto ){
 	
 		Object[] parametros;
-        String query = "INSERT INTO repuesta " +
+        String query = "INSERT INTO respuesta " +
                        "(id_evaluacion,id_plantilla,id_punto_evaluacion," + 
 					   "cumple_punto) " +
 					   "VALUES(?,?,?,?) "; 
         
-		int cumple = 0;
-		if(cumplePunto){
-		   cumple = 1;
-		}
+
+		int cumple = cumplePunto ? 1 : 0;
 		
         parametros = new Object[]{idEval,idPlant,idPtoEva,cumple};
         
@@ -27,9 +27,53 @@ public class RespuestaDA{
         }catch(Exception ex){
         	System.out.println("Error: " + ex.getMessage());
         }
-		finally{
-		   //DBManager.closeDBConnection();
-		}	    
 	}
 
+   public void cargarRespuestas(Evaluacion unaEv){
+        ResultSet result;
+        Object[] parametros;
+
+        String query = "SELECT id_punto_evaluacion, cumple_punto " +
+                       "FROM respuesta " +
+                       "WHERE id_evaluacion = ? " +
+                       "  AND id_plantilla = ?";
+
+        parametros = new Object[]{unaEv.getId(), unaEv.getIdPlantilla()};
+
+        try{
+            DBManager.openDBConnection();
+			result = DBManager.ejecutarQuery(query, parametros);
+
+            while(result.next()){			                
+				int id = result.getInt("id_punto_evaluacion"); 
+                boolean cumple = result.getInt("cumple_punto") == 1;
+
+                unaEv.crearRespuesta(id, cumple);
+			}
+        }
+        catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+   public void reclamarRespuesta(int idEv, int idPlant, int idResp, String coment){
+       Object[] parametros;
+
+       String query = "UPDATE respuesta " // Respuesta
+                       + "SET es_reclamada = 1, " // 1 = true
+                       + "    comentario=  ? "
+                       + "WHERE id_evaluacion = ? "
+                       + "  AND id_plantilla = ? "
+                       + "  AND id_punto_evaluacion = ?";
+
+       parametros = new Object[]{coment, idEv, idPlant, idResp};
+
+       try {
+            DBManager.openDBConnection();
+			DBManager.ejecutarUpdate(query, parametros);
+       }
+       catch(Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+       }
+   }
 }
