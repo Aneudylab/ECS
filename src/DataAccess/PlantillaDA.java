965d4510a75e7c085a21c84f0246c0258e00199c
Aneudy Labour <aneudylab@gmail.com>
2013-11-21 10:28:11 -0400

    Se actualizo PlantillaDA
    Se agregaron los metodos para obtener plantillas segun id de Evaluacion.

--- src/DataAccess/PlantillaDA.java
+++ src/DataAccess/PlantillaDA.java
@@ -3,6 +3,7 @@ package DataAccess;
 import java.sql.ResultSet;
 import java.util.Date;
 import domain.Plantilla;
+import domain.Evaluacion;
 
 /**
  * Data Access para plantilla
@@ -57,4 +58,60 @@ public class PlantillaDA{
 		    return plantilla;
 		}
 	}
+    
+    /**
+     * Lee una plantilla de la BD según el id de evaluación en que se usó
+	 * @param idEv id de la evaluación en que se usó la plantilla...
+     *  
+	 * @return Plantilla
+     **/
+    public Plantilla leerPlantilla(int idEv) {
+        String query = "SELECT id_plantilla FROM evaluacion WHERE id_evaluacion = ?";
+	    Object[] parametros;
+		PuntoEvaluacionDA puntoEvaDA = new PuntoEvaluacionDA();
+		Plantilla plantilla = null;
+        
+        parametros = new Object[]{idEv};
+        try {
+            DBManager.openDBConnection();
+            ResultSet result = DBManager.ejecutarQuery(query, parametros);
+
+            if (result.next()) {
+		      int idPlan = result.getInt("id");
+			  plantilla.setId(idPlan);
+			  puntoEvaDA.leerPuntosEvaluacion(plantilla);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return plantilla;
+    }
+
+    /**
+     * Lee una plantilla sin los puntos de evaluación desde la BD según 
+     * el id de la evaluación
+	 * @param idEv id de la evaluación en que se usó la plantilla...
+     *  
+	 * @return Plantilla
+     **/
+    public Plantilla leerSoloPlantilla(int idEv){
+        String query = "SELECT id_plantilla FROM evaluacion WHERE id_evaluacion = ?";
+	    Object[] parametros;
+		PuntoEvaluacionDA puntoEvaDA = new PuntoEvaluacionDA();
+		Plantilla plantilla = null;
+        
+        parametros = new Object[]{idEv};
+        try {
+            DBManager.openDBConnection();
+            ResultSet result = DBManager.ejecutarQuery(query, parametros);
+
+            if (result.next()) {
+		      int idPlan = result.getInt("id");
+			  plantilla.setId(idPlan);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return plantilla;
+    }
 } 
