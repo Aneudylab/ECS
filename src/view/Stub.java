0aba3ccf6b4f6dc82dc26f24847ff8eb2ceac693
LuisaIsabel <a_nyelia@hotmail.com>
2013-11-25 15:16:25 +0100

    Update Stub.java

--- src/view/Stub.java
+++ src/view/Stub.java
@@ -1,215 +1,59 @@
 package view;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+public class Stub {
 
-import domain.ControladorSesion;
+	public static HashMap<Integer, String> darRepresentantes() {
 
-
-
-public class MainWindow extends JFrame implements ActionListener {
-
-	// -----------------------------------------------------------------
-	// Constantes
-	// -----------------------------------------------------------------
-
-	private static final String CREAR_PLANTILLA = "crearPantillaEvaluacion";
-	private static final String GENERAR_REPORTE = "generarReporte";
-	private static final String EVALUAR_REPRESENTANTE = "evaluarRepresentante";
-	private static final String ACTUALIZAR_EVALUACION = "actualizarEvaluacion";
-	private static final String REVISAR_EVALUACION = "revisarEvaluacion";
-	private static final String CREAR_USUARIO = "crearUsuario";
-
-	// -----------------------------------------------------------------
-	// Atributos
-	// -----------------------------------------------------------------
-	
-	private PanelImagen panelImagen;
-	// -----------------------------------------------------------------
-	// Atributos de la Interfaz
-	// -----------------------------------------------------------------
-
-	private JButton btnCrearPlantillaEvaluacion;
-	private JButton btnGenerarReporte;
-	private JButton btnEvaluarReprecentante;
-	private JButton btnActualizarEvaluacion;
-	private JButton btnRevisarEvaluacion;
-	private JButton btnCrearUsuarios;
-	private JLabel 	lblBienvenido;
-
-	// -----------------------------------------------------------------
-	// Controladores
-	// -----------------------------------------------------------------
-    private ControladorSesion cSesion;
-
-	public MainWindow() {
-        // Controlador
-        cSesion = new ControladorSesion();
-
-		setSize(790, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new GridBagLayout());
-		setTitle("Sistema de Evaluacion  ");
-		setResizable(true);
-		setLocationRelativeTo(null);
-
-		panelImagen = new PanelImagen();
-		GridBagConstraints gbc = new GridBagConstraints();
-		// orientacion del Layout
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.gridy = 0;
-		gbc.gridx = 0;
-		// Definir espacio entre componentes
-		gbc.insets = new Insets(5, 18, 5, 18);
-		add(panelImagen, gbc);
+		HashMap<Integer, String> testMap = new HashMap<Integer, String>();
 		
-		
-		
-		btnCrearPlantillaEvaluacion = new JButton("Crear Plantilla    ", null);
-		btnCrearPlantillaEvaluacion.setBounds(30, 100, 90, 30);
-		gbc.gridy = 2;
-		gbc.insets = new Insets(5, 18, 5, 18);
-		btnCrearPlantillaEvaluacion.setActionCommand(CREAR_PLANTILLA);
-		btnCrearPlantillaEvaluacion.addActionListener(this);
-		add(btnCrearPlantillaEvaluacion, gbc);
-
-		btnGenerarReporte = new JButton("Generar Reporte ");
-		btnGenerarReporte.setBounds(100, 100, 100, 100);
-		gbc.gridy = 3;
-		btnGenerarReporte.setActionCommand(GENERAR_REPORTE);
-		btnGenerarReporte.addActionListener(this);
-		add(btnGenerarReporte, gbc);
-
-		btnEvaluarReprecentante = new JButton("Evaluar Representante ");
-		btnEvaluarReprecentante.setBounds(100, 100, 100, 100);
-		gbc.gridy = 4;
-		btnEvaluarReprecentante.setActionCommand(EVALUAR_REPRESENTANTE);
-		btnEvaluarReprecentante.addActionListener(this);
-		add(btnEvaluarReprecentante, gbc);
-
-		btnActualizarEvaluacion = new JButton("Actualizar Evaluacion ");
-		btnActualizarEvaluacion.setBounds(100, 100, 100, 100);
-		gbc.gridy = 5;
-		btnActualizarEvaluacion.setActionCommand(ACTUALIZAR_EVALUACION);
-		btnActualizarEvaluacion.addActionListener(this);
-		add(btnActualizarEvaluacion, gbc);
-
-		btnRevisarEvaluacion = new JButton("Revisar Evaluacion ");
-		btnRevisarEvaluacion.setBounds(100, 100, 100, 100);
-		gbc.gridy = 6;
-		btnRevisarEvaluacion.setActionCommand(REVISAR_EVALUACION);
-		btnRevisarEvaluacion.addActionListener(this);
-		add(btnRevisarEvaluacion, gbc);
-
-		btnCrearUsuarios = new JButton("Crear Usuario");
-		btnCrearUsuarios.setBounds(100, 100, 100, 100);
-		gbc.gridy = 7;
-		btnCrearUsuarios.setActionCommand(CREAR_USUARIO);
-		btnCrearUsuarios.addActionListener(this);
-		add(btnCrearUsuarios, gbc);
-		
-		lblBienvenido = new JLabel("Usuario	: "+ cSesion.getUsuarioActual().getNombre() + " " + cSesion.getUsuarioActual().getRol());
-		gbc.gridy = 8;
-		add(lblBienvenido, gbc);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent evento) {
-		// TODO Apendice de metodo generado automaticamente
-		String comando = evento.getActionCommand();
-
-		if (CREAR_PLANTILLA.equals(comando)) {
-			CrearNuevaPlantilla();
+		int i = 1;
+		while(i <= 18){
+			testMap.put(i, "  05-11-2013     Jairis Rosario                       ");
+			i++;
 		}
-		
-		if (GENERAR_REPORTE.equals(comando)) {
-			JOptionPane.showMessageDialog(this, "En Construccion..... ",
-					"Revisar reporte ", JOptionPane.INFORMATION_MESSAGE);
-		}
-		if (EVALUAR_REPRESENTANTE.equals(comando)) {
-			evaluarRepresentante();
-		}
-		if (ACTUALIZAR_EVALUACION.equals(comando)) {
-			actualizarEvaluacion(); // agregado
-		}
-		if (REVISAR_EVALUACION.equals(comando)) {
-			JOptionPane.showMessageDialog(this, "En Construccion..... ",
-					"Revisar Evaluacion", JOptionPane.INFORMATION_MESSAGE);
-		}
-		if (CREAR_USUARIO.equals(comando)) {
-			JOptionPane.showMessageDialog(this, "En Construccion..... ",
-					"Crear Usuario ",
-					JOptionPane.INFORMATION_MESSAGE);
-		}
+
+		return testMap;
 
 	}
 	
-	private void CrearNuevaPlantilla (){
-	   
-	   boolean esAdmin = cSesion.validarEsAdministrador();
-	   
-	   if(esAdmin){
-	      CrearPlantillaForm plantilla = new CrearPlantillaForm(cSesion.getUsuarioActual().getID());
-		  plantilla.mostrar();
-		  ocultar();
-		  
-	   }
-	   else{
-	      JOptionPane.showMessageDialog(null,
-						               "Usted no tiene privilegios para crear plantilla",
-						               "Usuario sin privilegio", 
-									   JOptionPane.ERROR_MESSAGE);	
-	   }
-	}
-	private void evaluarRepresentante(){
-		boolean esSup  = cSesion.validarEsSupervisor();
+	public static ArrayList<HashMap<String, String> > darEvaluacion() {
+
+		HashMap<String, String> testMap = new HashMap<String, String>();
+		ArrayList<HashMap<String, String> > testMapa = new ArrayList<HashMap<String, String> >();
 		
-		if(esSup){
-			EvaluarRepresentanteForm evaluar = new EvaluarRepresentanteForm();
-			evaluar.mostrar();
-			ocultar();
-			
-		}else{
-			JOptionPane.showMessageDialog(null,
-						               "Usted no tiene privilegios para evaluar representantes",
-						               "Usuario sin privilegio", 
-									    JOptionPane.ERROR_MESSAGE);	
+		int i = 0;
+		while(i < 9){
+			testMap.put(""+i+" ", "Fue Exelente su servicio con el cliente 	?     si      pienso que merezco mas puntos");
+			testMapa.add(testMap);
+			i++;
 		}
-	
+		return testMapa;
 	}
 	
-	// Agregado nuevo
+	public static int guardarEvaluacion(int idReps, HashMap<Integer, Boolean> Respuestas){
+		return 1;
+	}
 	
-	private void actualizarEvaluacion(){
-	    boolean esSup = cSesion.validarEsSupervisor();
+	
+	public static ArrayList<HashMap<String, String> > darEvaluacion6() {
 
-	    if(esSup){
-	    	ActualizarEvaluacionForm actualizar = new ActualizarEvaluacionForm();
-	        actualizar.mostrar();
-	        this.ocultar();
-	    }
-	    else{
-	    	JOptionPane.showMessageDialog(null,
-		                               "Usted no tiene privilegios para actualizar evaluaciones",
-		                               "Usuario sin privilegio", 
-					                    JOptionPane.ERROR_MESSAGE);
-	    }
+		
+		ArrayList<HashMap<String, String> > testMapa = new ArrayList<HashMap<String, String> >();
+		
+		int i = 0;
+		while(i < 12 ){
+			HashMap<String, String> testMap = new HashMap<String, String>();
+			testMap.put("id",""+i);
+			testMap.put("pto","Fue Exelente su servicio con el cliente mientras coversaba?");
+			testMap.put("resp","No");
+			testMap.put("coment","Pienso que merezco mas puntos dfdhgfdhgd fjfdhhsjkg gfdhgjk jshgjdhg");
+			testMapa.add(testMap);
+			i++;
+		}
+		return testMapa;
 	}
 
-	
-	private void ocultar(){
-	   this.setVisible(false);
-    }
-	// Metodo que hace visible la ventana principal
-	public void mostrar(){
-	   this.setVisible(true);
-	}
 }
