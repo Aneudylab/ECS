718c2de49a70df59e7b9637d672a597e600db863
Aneudy Labour <aneudylab@gmail.com>
2013-12-03 10:25:23 -0400

    Respuesta actualizada para CU005

--- src/domain/Respuesta.java
+++ src/domain/Respuesta.java
@@ -3,36 +3,40 @@ package domain;
 
 
 public class Respuesta{
-   
-	private int num;
-    private boolean cumple;
-	private String comentario ;
 
-    
+    private int num;
+    private boolean cumple;
+    private String comentario ;
+
+
     public Respuesta(){}
 
-	public Respuesta(int tmpNum,boolean tmpCumple){
-	   num = tmpNum;
-	   cumple = tmpCumple;
-	}
-	
-	public Respuesta (int tmpNum, String comment){
-		
-		this.num = tmpNum;
-		this.comentario = comment;
-	}
-	
-	public int getID(){
-	  return num;
-	}
-    
-	public boolean getCumplePunto(){
-	  return cumple;
-	}
-	
-	public String getComentario(){
-	
-		return this.comentario;
-	}
-	
+    public Respuesta(int tmpNum, boolean tmpCumple){
+        num = tmpNum;
+        cumple = tmpCumple;
+    }
+
+    public Respuesta (int tmpNum, String comment){
+        num = tmpNum;
+        comentario = comment;
+    }
+
+    public Respuesta(int tmpNum, boolean tmpCumple, String comment){
+        num = tmpNum;
+        cumple = tmpCumple;
+        comentario = comment;
+    }
+
+    public int getID(){
+        return num;
+    }
+
+    public boolean getCumplePunto(){
+        return cumple;
+    }
+
+    public String getComentario(){
+        return this.comentario;
+    }
+
 }
