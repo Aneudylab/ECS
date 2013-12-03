1dd6b8e67205dd6dd10b54df7c88451999cd816a
Aneudy Labour <aneudylab@gmail.com>
2013-12-03 10:25:58 -0400

    Evaluacion actualizada para CU005

--- src/domain/Evaluacion.java
+++ src/domain/Evaluacion.java
@@ -5,27 +5,28 @@ import java.util.Date;
 import DataAccess.RespuestaDA;
 
 public class Evaluacion{
-  
-   private int evaluacionid;
-   private Plantilla unaPlantilla;
-   private Date fechaCreada;
-   private ArrayList<Respuesta> listaRespuestas;
-  
-   //---------------------------------------------------
-   //	Constructores
-   //------------------------------------------------
-   public Evaluacion(){}
 
-   public Evaluacion(Plantilla tmpPlantilla){
-		unaPlantilla = tmpPlantilla;
-		listaRespuestas = new ArrayList<Respuesta>();
+    private int evaluacionid;
+    private Plantilla unaPlantilla;
+    private Date fechaCreada;
+    private ArrayList<Respuesta> listaRespuestas;
+    private Representante representante;
+
+    //---------------------------------------------------
+    //	Constructores
+    //------------------------------------------------
+    public Evaluacion(){}
+
+    public Evaluacion(Plantilla tmpPlantilla){
+        unaPlantilla = tmpPlantilla;
+        listaRespuestas = new ArrayList<Respuesta>();
     }
-	
-	public Evaluacion(Plantilla tmpPlantilla, ArrayList<Respuesta> resp){
-	    unaPlantilla = tmpPlantilla;
-		listaRespuestas = resp;	    
-	}
-	
+
+    public Evaluacion(Plantilla tmpPlantilla, ArrayList<Respuesta> resp){
+        unaPlantilla = tmpPlantilla;
+        listaRespuestas = resp;	    
+    }
+
     public Evaluacion(int id, Date fechaCreada){
         this.evaluacionid = id;
         this.fechaCreada = fechaCreada;
@@ -37,73 +38,84 @@ public class Evaluacion{
         this.unaPlantilla = unaPlant;
         listaRespuestas = new ArrayList<Respuesta>();
     } 
-   //---------------------------------------------------
-   //	Metodos
-   //------------------------------------------------
-   public int getId(){
-      return evaluacionid;
-   } 
-   
-   public int getIdPlantilla(){
-      return unaPlantilla.getId();
-   }
-	
-   public void crearRespuesta(int tmpNum, boolean tmpCumple){
-      listaRespuestas.add(new Respuesta(tmpNum,tmpCumple));
-   }
-   
-   public void setID (int tmpID){
-      evaluacionid = tmpID;
-   }
-   
-   public void guardarRespuestas(){
-     RespuestaDA unaResDA = new RespuestaDA();
-	 
-     for(Respuesta tmpRes:listaRespuestas){
-	    int idPunto = tmpRes.getID();
-		boolean cumple = tmpRes.getCumplePunto();
-		int idPlantilla = unaPlantilla.getId();
-		
-		unaResDA.guardarRespuesta(evaluacionid,unaPlantilla.getId(),
-		                          idPunto,cumple);
-	 }
-   }
 
-   public Date getFechaCreada(){
-       return this.fechaCreada;
-   }
+    public Evaluacion(int id, Date fechaCreada, Representante representante){
+        this.evaluacionid = id;
+        this.fechaCreada = fechaCreada;
+        this.representante = representante;
+        listaRespuestas = new ArrayList<Respuesta>();
+    }
+    //---------------------------------------------------
+    //	Metodos
+    //------------------------------------------------
+    public int getId(){
+        return evaluacionid;
+    } 
 
-   public int getIdRespuesta(int index){
-       if(index < 0 || index > listaRespuestas.size()) return -1;
-       return listaRespuestas.get(index).getID();
-   }
+    public void setID (int tmpID){
+        evaluacionid = tmpID;
+    }
 
-   public boolean getRespuesta(int id){
-       for(Respuesta resp: listaRespuestas){
-           if(resp.getID() == id) return resp.getCumplePunto();
-       }
+    public int getIdPlantilla(){
+        return unaPlantilla.getId();
+    }
 
-       return false;
-   }
+    public void crearRespuesta(int tmpNum, boolean tmpCumple){
+        listaRespuestas.add(new Respuesta(tmpNum,tmpCumple));
+    }
 
-   public String getComentario(int id){
-       for(Respuesta resp: listaRespuestas){
-           if(resp.getID() == id) return resp.getComentario();
-       }
+    public void crearRespuesta(int tmpNum, String tmpComent){
+        listaRespuestas.add(new Respuesta(tmpNum,tmpComent));
+    }
 
-       return "";
-   }
+    public void crearRespuesta(int tmpNum, boolean cumple, String tmpComent){
+        listaRespuestas.add(new Respuesta(tmpNum, cumple, tmpComent));
+    }
 
-   public String getPuntoEvaluacion(int id) {
-       return unaPlantilla.getPuntoEvaluacion(id);
-   }
+    public void guardarRespuestas(){
+        RespuestaDA unaResDA = new RespuestaDA();
 
-   public int contarRespuestas(){
-       return listaRespuestas.size();
-   }
+        for(Respuesta tmpRes:listaRespuestas){
+            int idPunto = tmpRes.getID();
+            boolean cumple = tmpRes.getCumplePunto();
+            int idPlantilla = unaPlantilla.getId();
 
-   public void crearRespuesta(int tmpNum, String tmpComent){
-      listaRespuestas.add(new Respuesta(tmpNum,tmpComent));
-   }
-  
+            unaResDA.guardarRespuesta(evaluacionid,unaPlantilla.getId(),
+                    idPunto,cumple);
+        }
+    }
+
+    public Date getFechaCreada(){
+        return this.fechaCreada;
+    }
+
+    public int getIdRespuesta(int index){
+        if(index < 0 || index > listaRespuestas.size()) return -1;
+        return listaRespuestas.get(index).getID();
+    }
+
+    public boolean getRespuesta(int id){
+        for(Respuesta resp: listaRespuestas){
+            if(resp.getID() == id) return resp.getCumplePunto();
+        }
+
+        return false;
+    }
+
+    public String getComentario(int id){
+        for(Respuesta resp: listaRespuestas){
+            if(resp.getID() == id) return resp.getComentario();
+        }
+
+        return "";
+    }
+
+    public String getPuntoEvaluacion(int id) {
+        return unaPlantilla.getPuntoEvaluacion(id);
+    }
+
+    public int contarRespuestas(){
+        return listaRespuestas.size();
+    }
+
 }
