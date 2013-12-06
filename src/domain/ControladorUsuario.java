bbcad0fc3ae840eb20c519295034b584765b4e69
ericherrera <ericmanuel.herrera@gmail.com>
2013-12-06 01:14:54 -0400

    Se creo la Clase ControladorUsuario
    Esta clase es primera creada y usada en este CU006

---
+++ src/domain/ControladorUsuario.java
@@ -0,0 +1,62 @@
+
+package domain;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import DataAccess.RolDA;
+import DataAccess.SupervisorDA;
+
+
+public class ControladorUsuario{
+
+  public HashMap<Integer,String> obtenerListaSupervisores(){
+    
+	SupervisorDA unSuperDA = new SupervisorDA();
+	ArrayList<Supervisor> tmpSupervisores = unSuperDA.leerSupervisores();
+    
+	HashMap<Integer,String> listaSuper = new HashMap<Integer,String>();
+	listaSuper.put(0,"");
+	
+    for (Supervisor tmpSpv : tmpSupervisores){
+	   
+	    int id = tmpSpv.getId();
+		String nombre = tmpSpv.getNombre();
+		
+		listaSuper.put(id,nombre);
+	}	
+	
+	return listaSuper;
+  }
+  
+  public HashMap<Integer,String> obtenerListaRoles(){
+	
+	RolDA unRolDA = new RolDA();
+	ArrayList<Rol> tmpRoles = unRolDA.leerRoles();
+    
+	HashMap<Integer,String> listaRoles = new HashMap<Integer,String>();
+	listaRoles.put(0,"");
+	
+    for (Rol tmpRol : tmpRoles){
+	   
+	    int id = tmpRol.getId();
+		String descripcion = tmpRol.getDescripcion();
+		
+		listaRoles.put(id,descripcion);
+	}	
+	
+	return listaRoles;  
+  }
+  
+  public int crearUsuario (String nombre, int rol, String clave, 
+                           String nomUsr, int idSuper){
+
+      Usuario usuActual = new ControladorSesion().getUsuarioActual();
+	  
+	  Administrador unAdmin = new Administrador();
+	  unAdmin.copiar(usuActual); 
+	  int id = unAdmin.crearUsuario(nombre,rol,clave,nomUsr,idSuper);
+	  
+	  return id;
+   }
+   
+}
